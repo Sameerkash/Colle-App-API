c6d533c58d73a8bdb8785309645b944fab57c4ce
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-07-01 18:28:09 +0530

    added hashing

--- dist/resolvers/mutations.js
+++ dist/resolvers/mutations.js
@@ -46,11 +46,15 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var bcrypt_1 = __importDefault(require("bcrypt"));
 var Mutation = {
     signUp: function (parent, args, ctx, info) {
         return __awaiter(this, void 0, void 0, function () {
-            var userCheck, user;
+            var userCheck, hashedPassword, user;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0: return [4 /*yield*/, ctx.prisma.user({
@@ -61,9 +65,13 @@ var Mutation = {
                         if (userCheck) {
                             throw new Error("Email is already registered");
                         }
+                        return [4 /*yield*/, bcrypt_1.default.hash(args.data.password, 12)];
+                    case 2:
+                        hashedPassword = _a.sent();
+                        args.data.password = hashedPassword;
                         console.log(args.data);
                         return [4 /*yield*/, ctx.prisma.createUser(__assign({}, args.data))];
-                    case 2:
+                    case 3:
                         user = _a.sent();
                         return [2 /*return*/, user];
                 }
--- src/resolvers/mutations.ts
+++ src/resolvers/mutations.ts
@@ -1,5 +1,6 @@
 import { Context } from "graphql-yoga/dist/types";
 import { Args } from "prisma-client-lib/dist/types";
+import bcrypt from "bcrypt";
 
 const Mutation = {
   async signUp(parent: any, args: Args, ctx: Context, info: any) {
@@ -9,6 +10,9 @@ const Mutation = {
     if (userCheck) {
       throw new Error("Email is already registered");
     }
+    const hashedPassword = await bcrypt.hash(args.data.password, 12);
+    args.data.password = hashedPassword;
+
     console.log(args.data);
     const user = await ctx.prisma.createUser({
       ...args.data,
