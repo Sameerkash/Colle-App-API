5480e708882a1321d3ca7c8bdbfeadc795173b00
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2020-07-01 16:02:08 +0530

    added first mutation

--- dist/resolvers/mutations.js
+++ dist/resolvers/mutations.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -39,14 +50,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Mutation = {
     createUser: function (parent, args, ctx, info) {
         return __awaiter(this, void 0, void 0, function () {
+            var user;
             return __generator(this, function (_a) {
                 switch (_a.label) {
-                    case 0: return [4 /*yield*/, ctx.prisma.createUser({
-                            name: args.name,
-                            email: args.email,
-                            password: args.password,
-                        })];
-                    case 1: return [2 /*return*/, _a.sent()];
+                    case 0:
+                        console.log(args);
+                        return [4 /*yield*/, ctx.prisma.createUser(__assign({}, args.data))];
+                    case 1:
+                        user = _a.sent();
+                        console.log(user);
+                        return [2 /*return*/, user];
                 }
             });
         });
--- src/resolvers/mutations.ts
+++ src/resolvers/mutations.ts
@@ -1,17 +1,14 @@
 import { Context } from "graphql-yoga/dist/types";
+import { getServers } from "dns";
 
 const Mutation = {
-  async createUser(
-    parent: any,
-    args: { name: String; email: String; password: String },
-    ctx: Context,
-    info: any
-  ) {
-    return await ctx.prisma.createUser({
-      name: args.name,
-      email: args.email,
-      password: args.password,
+  async createUser(parent: any, args: any, ctx: Context, info: any) {
+    console.log(args);
+    const user = await ctx.prisma.createUser({
+      ...args.data,
     });
+    console.log(user);
+    return user;
   },
 };
 
